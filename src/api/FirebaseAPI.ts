487d4ffd6f06be6413a9074bf512620d82ab3495
Richard <39134434+richardxgao@users.noreply.github.com>
2022-08-11 18:27:01 -0600

    Add uploadEmployeeFile

--- src/api/FirebaseAPI.ts
+++ src/api/FirebaseAPI.ts
@@ -1,31 +1,33 @@
-import { collection, getDocs, setDoc, doc, query, where, QuerySnapshot, DocumentData } from "firebase/firestore";
+import { collection, getDocs, setDoc, doc, query, where, DocumentData } from "firebase/firestore";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { db } from "../providers/FirebaseProvider";
 import { Employee } from "../App";
 
 const employeesRef = collection(db, "employees");
+const storage = getStorage();
 
 export const checkIfEmployeeExists = async (employee: Employee): Promise<boolean> => {
   const q = query(employeesRef, where("email", "==", employee.email));
   const querySnapshot = await getDocs(q);
-  if (querySnapshot.size > 0) {
-    console.log("Employee with email already exists.");
-    return true;
-  }
-  return false;
+  return querySnapshot.size > 0;
 };
 
-export const createEmployee = async (employee: Employee): Promise<boolean> => {
-  const exists = await checkIfEmployeeExists(employee);
-  if (!exists) {
-    const newDocRef = doc(employeesRef);
-    await setDoc(newDocRef, { ...employee, id: newDocRef.id });
-    return true;
-  }
-  return false;
+export const createEmployee = async (employee: Employee): Promise<void> => {
+  const newDocRef = doc(employeesRef);
+  await setDoc(newDocRef, { ...employee, id: newDocRef.id });
 };
 
 export const getEmployees = async (): Promise<DocumentData[]> => {
   const data = await getDocs(employeesRef);
   const employees: DocumentData[] = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
   return employees;
+};
+
+export const uploadEmployeeFile = async (file: File, employee: Employee): Promise<string> => {
+  const storageRef = ref(storage, `employees/${employee.email}/${file.name}`);
+
+  await uploadBytes(storageRef, file);
+  const downloadURL = await getDownloadURL(storageRef);
+
+  return downloadURL;
 };
