d861deeb7b06f67dde0f9bfd2fd1d3fcd4458009
Richard <39134434+richardxgao@users.noreply.github.com>
2022-08-11 21:32:14 -0600

    Create SubmitForm.tsx

---
+++ src/components/SubmitForm.tsx
@@ -0,0 +1,141 @@
+import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
+import { SelectChangeEvent } from "@mui/material/Select";
+import { ChangeEvent, useState } from "react";
+import { createEmployee, checkIfEmployeeExists, uploadEmployeeFile } from "../api/FirebaseAPI";
+import CustomAlert from "./CustomAlert";
+import { Employee } from "../api/FirebaseAPI";
+
+const validateEmail = (email: string): boolean => {
+  return /\S+@\S+\.\S+/.test(email);
+};
+
+const validateForm = (employee: Employee): boolean => {
+  if (employee.name.length === 0) {
+    return false;
+  }
+  if (employee.id.length === 0) {
+    return false;
+  }
+  if (employee.department.length === 0) {
+    return false;
+  }
+  if (employee.employmentStatus.length === 0) {
+    return false;
+  }
+  if (!validateEmail(employee.email)) {
+    return false;
+  }
+  return true;
+};
+
+const emptyEmployee: Employee = {
+  name: "",
+  id: "",
+  department: "",
+  employmentStatus: "",
+  email: "",
+};
+
+const SubmitForm = () => {
+  const [employee, setEmployee] = useState<Employee>(emptyEmployee);
+  const [file, setFile] = useState<null | File>(null);
+  const [validEmail, setValidEmail] = useState<boolean>(true);
+  const [alert, setAlert] = useState<"success" | "invalid" | "duplicate" | null>(null);
+
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+    setEmployee({ ...employee, [e.currentTarget.name]: e.currentTarget.value });
+  };
+
+  const handleEmploymentStatusChange = (e: SelectChangeEvent): void => {
+    setEmployee({ ...employee, employmentStatus: e.target.value });
+  };
+
+  const handleSubmit = async () => {
+    const validForm = validateForm(employee);
+    if (!validForm) {
+      setAlert("invalid");
+      return;
+    }
+    const exists: boolean = await checkIfEmployeeExists(employee);
+    if (exists) {
+      setAlert("duplicate");
+      return;
+    }
+    if (file !== null && file !== undefined) {
+      const downloadURL = await uploadEmployeeFile(file, employee);
+      await createEmployee({ ...employee, additionalDocuments: { fileName: file.name, url: downloadURL } });
+    } else {
+      await createEmployee(employee);
+    }
+    console.log("Success");
+    setAlert("success");
+  };
+
+  const handleClear = () => {
+    setFile(null);
+    setEmployee(emptyEmployee);
+    setAlert(null);
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      {alert && <CustomAlert type={alert} />}
+      <form className="rounded-xl flex flex-col justify-center bg-white gap-4 p-8">
+        <h1>Identification Information</h1>
+        <TextField
+          required
+          name="name"
+          label="Full Name"
+          variant="standard"
+          value={employee.name}
+          onChange={handleOnChange}
+        />
+        <TextField required name="id" label="ID" variant="standard" value={employee.id} onChange={handleOnChange} />
+        <TextField
+          required
+          name="department"
+          label="Department"
+          variant="standard"
+          value={employee.department}
+          onChange={handleOnChange}
+        />
+        <FormControl required variant="standard">
+          <InputLabel>Employment Status</InputLabel>
+          <Select value={employee.employmentStatus} label="Employment Status" onChange={handleEmploymentStatusChange}>
+            <MenuItem value={1}>Employed</MenuItem>
+            <MenuItem value={0}>Unemployed</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField
+          error={!validEmail}
+          required
+          name="email"
+          label="Email"
+          variant="standard"
+          value={employee.email}
+          onChange={(e) => {
+            const isValidEmail: boolean = validateEmail(e.target.value);
+            setValidEmail(isValidEmail);
+            handleOnChange(e);
+          }}
+        />
+        <h1 className="mt-4">Additional Documents</h1>
+        <Button color="secondary" variant="contained" component="label">
+          Upload (pdf)
+          <input hidden accept="application/pdf" type="file" onChange={(e: any) => setFile(e.target.files[0])} />
+        </Button>
+        <p className="text-center">{file?.name}</p>
+        <div className="flex justify-items-stretch mt-8 gap-4">
+          <Button className="flex-1" variant="contained" onClick={handleSubmit}>
+            Submit
+          </Button>
+          <Button className="flex-1" variant="contained" onClick={handleClear}>
+            Clear
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SubmitForm;
