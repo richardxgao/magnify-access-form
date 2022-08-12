c5254aad4debc3152f9ea1b8dee5896e4b2603c5
Richard <39134434+richardxgao@users.noreply.github.com>
2022-08-11 18:27:22 -0600

    Create separate alerts for success, invalid, duplicate

--- src/components/CustomAlert.tsx
+++ src/components/CustomAlert.tsx
@@ -1,15 +1,28 @@
-import { Alert, AlertTitle } from "@mui/material";
+import { Alert } from "@mui/material";
 
-const CustomAlert = ({ type }: { type: boolean }) => {
-  return type ? (
-    <div className="absolute bottom-4">
-      <Alert severity="success">Successfully submitted</Alert>
-    </div>
-  ) : (
-    <div className="absolute bottom-4">
-      <Alert severity="error">Error: Form is invalid.</Alert>
-    </div>
-  );
+const CustomAlert = ({ type }: { type: "success" | "invalid" | "duplicate" | null }) => {
+  let AlertType = <div></div>;
+  if (type === "success") {
+    AlertType = (
+      <div className="absolute bottom-4">
+        <Alert severity="success">Successfully submitted.</Alert>
+      </div>
+    );
+  } else if (type === "invalid") {
+    AlertType = (
+      <div className="absolute bottom-4">
+        <Alert severity="error">Error: Form is invalid.</Alert>
+      </div>
+    );
+  } else if (type === "duplicate") {
+    AlertType = (
+      <div className="absolute bottom-4">
+        <Alert severity="error">Duplicate Employee.</Alert>
+      </div>
+    );
+  }
+
+  return AlertType;
 };
 
 export default CustomAlert;
