f55457d067a61590b33707f4c4be57449f3ea5bd
Richard <39134434+richardxgao@users.noreply.github.com>
2022-08-19 17:28:38 -0600

    Use switch statement instead of verbose if-else chain

--- src/components/CustomAlert.tsx
+++ src/components/CustomAlert.tsx
@@ -1,28 +1,28 @@
 import { Alert } from "@mui/material";
 
-const CustomAlert = ({ type }: { type: "success" | "invalid" | "duplicate" | null }) => {
-  let AlertType = <div></div>;
-  if (type === "success") {
-    AlertType = (
-      <div className="absolute bottom-[76px]">
-        <Alert severity="success">Successfully submitted.</Alert>
-      </div>
-    );
-  } else if (type === "invalid") {
-    AlertType = (
-      <div className="absolute bottom-[76px]">
-        <Alert severity="error">Error: Form is invalid.</Alert>
-      </div>
-    );
-  } else if (type === "duplicate") {
-    AlertType = (
-      <div className="absolute bottom-[76px]">
-        <Alert severity="error">Duplicate Employee.</Alert>
-      </div>
-    );
+const CustomAlert = ({ type }: { type: "success" | "invalid" | "duplicate" | null }): JSX.Element | null => {
+  switch (type) {
+    case "success":
+      return (
+        <Alert className="absolute bottom-[76px]" severity="success">
+          Successfully submitted.
+        </Alert>
+      );
+    case "invalid":
+      return (
+        <Alert className="absolute bottom-[76px]" severity="error">
+          Error: Form is invalid.
+        </Alert>
+      );
+    case "duplicate":
+      return (
+        <Alert className="absolute bottom-[76px]" severity="error">
+          Duplicate Employee.
+        </Alert>
+      );
+    default:
+      return null;
   }
-
-  return AlertType;
 };
 
 export default CustomAlert;
