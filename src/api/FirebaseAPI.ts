9c32bc241aa2fd963d1e661ba37c29a5413acddd
Richard <39134434+richardxgao@users.noreply.github.com>
2022-08-19 17:17:32 -0600

    Clean up and refactor checkIfEmployeeExists()

--- src/api/FirebaseAPI.ts
+++ src/api/FirebaseAPI.ts
@@ -20,17 +20,10 @@ const employeesRef = collection(db, "employees");
 const storage = getStorage();
 
 export const checkIfEmployeeExists = async (employee: Employee): Promise<boolean> => {
-  const q = query(employeesRef, where("email", "==", employee.email));
-  const querySnapshot = await getDocs(q);
-  if (querySnapshot.size > 0) {
-    return true;
-  }
-  const q2 = query(employeesRef, where("id", "==", employee.id));
-  const querySnapshot2 = await getDocs(q2);
-  if (querySnapshot2.size > 0) {
-    return true;
-  }
-  return false;
+  const employeeEmailQuery = await getDocs(query(employeesRef, where("email", "==", employee.email)));
+  const employeeIdQuery = await getDocs(query(employeesRef, where("id", "==", employee.id)));
+
+  return employeeEmailQuery.size > 0 || employeeIdQuery.size > 0;
 };
 
 export const createEmployee = async (employee: Employee): Promise<void> => {
@@ -41,13 +34,14 @@ export const createEmployee = async (employee: Employee): Promise<void> => {
 export const getEmployees = async (): Promise<DocumentData[]> => {
   const data = await getDocs(employeesRef);
   const employees: DocumentData[] = data.docs.map((doc) => ({ ...doc.data() }));
+
   return employees;
 };
 
 export const uploadEmployeeFile = async (file: File, employee: Employee): Promise<string> => {
   const storageRef = ref(storage, `employees/${employee.email}/${file.name}`);
-
   await uploadBytes(storageRef, file);
+
   const downloadURL = await getDownloadURL(storageRef);
 
   return downloadURL;
